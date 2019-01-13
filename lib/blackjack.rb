aa1e137b9e603c3add1520489a6239d2a3dc93ba
Isa Levine <isaacdlevine@gmail.com>
2019-01-13 00:57:15 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -58,10 +58,6 @@ def runner
   until cardfinal > 21
     cardfinal += hit?(10)
   end
-  if cardfinal == "30"
-  else
-    cardfinal = "30"
-  end
   display_card_total(cardfinal)
   end_game(cardfinal)
 end
